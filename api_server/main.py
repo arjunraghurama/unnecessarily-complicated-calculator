8ee66d130823345adc5f0af9ebea0367bed9df06
Arjun R <arjunr0019@gmail.com>
2026-04-23 00:10:38 +0530

    added valkey as caching layer

--- api_server/main.py
+++ api_server/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 from .schema import Operands
-from .logging.logger import logger
+from .log.logger import logger
 from contextlib import asynccontextmanager
 import boto3
 import json
 import os
+import valkey
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -18,9 +19,9 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 endpoint_url = (
-    "http://ministack:4566"
+    "http://localhost:4566"
     if os.getenv("ENVIRONMENT") == "test"
-    else "http://localhost:4566"
+    else "http://ministack:4566"
 )
 
 lambda_client = boto3.client(
@@ -31,50 +32,97 @@ lambda_client = boto3.client(
     region_name="us-east-1",
 )
 
+valkey_endpoint = (
+    "localhost"
+    if os.getenv("ENVIRONMENT") == "test"
+    else "valkey"
+)
+
+cache_client = valkey.Valkey(host="valkey", port=6379, db=0)
 
 @app.post("/add")
 def add(operands: Operands):
     logger.info("Add operation requested")
+    
+    # check if result is already in cache
+    if cache_client.exists(f"add_{operands.a}_{operands.b}"):
+        logger.info(f"Result found in cache for add operation {operands.a} + {operands.b}")
+        return {"result": int(cache_client.get(f"add_{operands.a}_{operands.b}"))}
+    
     response = lambda_client.invoke(
         FunctionName="basic-math-lambda-function",
         Payload=json.dumps({"operation": "add", "a": operands.a, "b": operands.b}),
     )
     logger.info(f"Lambda response: {response}")
     result = json.loads(response["Payload"].read())
+
+    # store result in cache with timeout of 10 minutes
+    cache_client.set(f"add_{operands.a}_{operands.b}", result["body"], ex=600)
+    logger.info(f"Result {result['body']} stored in cache for add operation {operands.a} + {operands.b}")
     return {"result": result["body"]}
 
 
 @app.post("/subtract")
 def subtract(operands: Operands):
     logger.info("Subtract operation requested")
+
+    # check if result is already in cache
+    if cache_client.exists(f"subtract_{operands.a}_{operands.b}"):
+        logger.info(f"Result found in cache for subtract operation {operands.a} - {operands.b}")
+        return {"result": int(cache_client.get(f"subtract_{operands.a}_{operands.b}"))}
+
     response = lambda_client.invoke(
         FunctionName="basic-math-lambda-function",
         Payload=json.dumps({"operation": "subtract", "a": operands.a, "b": operands.b}),
     )
     logger.info(f"Lambda response: {response}")
     result = json.loads(response["Payload"].read())
+
+    # store result in cache with timeout of 10 minutes
+    cache_client.set(f"subtract_{operands.a}_{operands.b}", result["body"], ex=600)
+    logger.info(f"Result {result['body']} stored in cache for subtract operation {operands.a} - {operands.b}")
     return {"result": result["body"]}
 
 
 @app.post("/multiply")
 def multiply(operands: Operands):
     logger.info("Multiply operation requested")
+
+    # check if result is already in cache
+    if cache_client.exists(f"multiply_{operands.a}_{operands.b}"):
+        logger.info(f"Result found in cache for multiply operation {operands.a} * {operands.b}")
+        return {"result": int(cache_client.get(f"multiply_{operands.a}_{operands.b}"))}
+
     response = lambda_client.invoke(
         FunctionName="basic-math-lambda-function",
         Payload=json.dumps({"operation": "multiply", "a": operands.a, "b": operands.b}),
     )
     logger.info(f"Lambda response: {response}")
     result = json.loads(response["Payload"].read())
+
+    # store result in cache with timeout of 10 minutes
+    cache_client.set(f"multiply_{operands.a}_{operands.b}", result["body"], ex=600)
+    logger.info(f"Result {result['body']} stored in cache for multiply operation {operands.a} * {operands.b}")
     return {"result": result["body"]}
 
 
 @app.post("/divide")
 def divide(operands: Operands):
     logger.info("Divide operation requested")
+
+    # check if result is already in cache
+    if cache_client.exists(f"divide_{operands.a}_{operands.b}"):
+        logger.info(f"Result found in cache for divide operation {operands.a} / {operands.b}")
+        return {"result": int(cache_client.get(f"divide_{operands.a}_{operands.b}"))}
+
     response = lambda_client.invoke(
         FunctionName="basic-math-lambda-function",
         Payload=json.dumps({"operation": "divide", "a": operands.a, "b": operands.b}),
     )
     logger.info(f"Lambda response: {response}")
     result = json.loads(response["Payload"].read())
+
+    # store result in cache with timeout of 10 minutes
+    cache_client.set(f"divide_{operands.a}_{operands.b}", result["body"], ex=600)
+    logger.info(f"Result {result['body']} stored in cache for divide operation {operands.a} / {operands.b}")
     return {"result": result["body"]}
